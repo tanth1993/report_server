5f59e15c480f3dd9cc870026a554c0eabbb9015e
Tan_Windows <tan.tranhoang@haravan.com>
2021-08-28 22:09:51 +0700

    change config 4

--- build/index.js
+++ build/index.js
@@ -3,17 +3,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = __importDefault(require("express"));
-var app = express_1.default();
-var port = 3099;
-app.get('/test', function (req, res) {
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+const port = 3099;
+app.get('/test', (req, res) => {
     res.send('tesst APi here!');
 });
-app.get('/', function (req, res) {
+app.get('/entry', (req, res) => {
+    res.send('entry API fucking here!');
+});
+app.get('/', (req, res) => {
     res.send('Hello World heheheheh!');
 });
-app.listen(port, function () {
-    console.log("Example app listening at http://localhost:" + port);
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
 });
 module.exports = app;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,9 @@ const port = 3099
 app.get('/test', (req, res) => {
     res.send('tesst APi here!')
 })
+app.get('/entry', (req, res) => {
+    res.send('entry API fucking here!')
+})
 app.get('/', (req, res) => {
     res.send('Hello World heheheheh!')
 })
